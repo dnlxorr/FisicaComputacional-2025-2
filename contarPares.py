c32e3835ee4b9e5654dba972d9021669b549162f
aearrieta <aearrieta2208@gmail.com>
2025-09-05 15:03:19 -0500

    contador de numeros enteros pares

---
+++ contarPares.py
@@ -0,0 +1,11 @@
+def cont_pares(numero):
+    contador = 0
+
+    for numero in range(numero):
+
+        if numero %2 != 0:
+            contador = contador + 1
+
+            print(contador)
+
+    cont_pares(100)
